14b8c9cd29f754faf37f6ca707095a6aa79d65a0
Higor Armani <higor.armani@gmail.com>
2020-08-11 22:50:37 -0300

    Criação do Formulario de Serviço e de Termos de Uso

--- src/components/service-form/ServiceForm.vue
+++ src/components/service-form/ServiceForm.vue
@@ -1,116 +1,155 @@
 <template>
-  <form>
-    <v-text-field
-      v-model="name"
-      :error-messages="nameErrors"
-      :counter="10"
-      label="Name"
-      required
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-      v-model="email"
-      :error-messages="emailErrors"
-      label="E-mail"
-      required
-      @input="$v.email.$touch()"
-      @blur="$v.email.$touch()"
-    ></v-text-field>
-    <v-select
-      v-model="select"
-      :items="items"
-      :error-messages="selectErrors"
-      label="Item"
-      required
-      @change="$v.select.$touch()"
-      @blur="$v.select.$touch()"
-    ></v-select>
-    <v-checkbox
-      v-model="checkbox"
-      :error-messages="checkboxErrors"
-      label="Do you agree?"
-      required
-      @change="$v.checkbox.$touch()"
-      @blur="$v.checkbox.$touch()"
-    ></v-checkbox>
+  <div>
+    <v-col cols="8">
+      <v-card class="mx-auto dg-card">
+        <form>
+          <v-list-item>
+            <v-list-item-content>
+              <v-icon large color="green darken-2">mdi-domain</v-icon>
+              <v-list-item-title class="headline">Cadastrar Serviço</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
 
-    <v-btn class="mr-4" @click="submit">submit</v-btn>
-    <v-btn @click="clear">clear</v-btn>
-  </form>
+          <v-text-field
+            v-model="formData.title"
+            class="dg-input"
+            :error-messages="titleErrors"
+            :counter="titleTotalCaracters"
+            label="Titulo do Anúncio"
+            prepend-icon="mdi-grease-pencil"
+            @input="$v.formData.title.$touch()"
+            @blur="$v.formData.title.$touch()"
+            required
+          />
+
+          <v-text-field
+            v-model="formData.openingHours"
+            class="dg-input"
+            label="Descrição de Dias e Horários de Funcionamento"
+            prepend-icon="mdi-calendar-clock"
+            @input="$v.formData.title.$touch()"
+            @blur="$v.formData.title.$touch()"
+          />
+
+          <v-file-input
+            v-model="formData.imagem"
+            class="dg-input"
+            accept="image/png, image/jpeg, image/bmp"
+            prepend-icon="mdi-camera"
+            label="Imagem do Anúncio"
+          />
+
+          <v-textarea
+            v-model="formData.description"
+            class="dg-input"
+            outlined
+            prepend-icon="mdi-pencil-box-outline"
+            name="input-7-4"
+            label="Escreva Sobre o Serviço Oferecido"
+          />
+
+          <TermsService />
+
+          <v-checkbox
+            v-model="formData.checkbox"
+            :error-messages="checkboxErrors"
+            label="Compreendo e aceito os Termos e Condições."
+            @change="$v.formData.checkbox.$touch()"
+            @blur="$v.formData.checkbox.$touch()"
+            :lazy-validation="true"
+            required
+          />
+
+          <v-card-actions>
+            <v-btn class="mr-4" @click.prevent="submit" color="success">Finalizar</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </form>
+      </v-card>
+    </v-col>
+  </div>
 </template>
 
 <script>
-  import { validationMixin } from 'vuelidate'
-  import { required, maxLength, email } from 'vuelidate/lib/validators'
+import { validationMixin } from "vuelidate"
+import { required, maxLength } from "vuelidate/lib/validators"
+import TermsService from "@/components/shared/terms-service/TermsService.vue"
+import ServiceProvider from "@/providers/services/service_provider"
 
-  export default {
-    mixins: [validationMixin],
-
-    validations: {
-      name: { required, maxLength: maxLength(10) },
-      email: { required, email },
-      select: { required },
-      checkbox: {
-        checked (val) {
-          return val
+export default {
+  components: {
+    TermsService,
+  },
+  mixins: [validationMixin],
+  validations() {
+    return {
+      formData: {
+        title: {
+          required,
+          maxLength: maxLength(this.titleTotalCaracters),
+        },
+        checkbox: {
+          checked(value) {
+            return value
+          },
         },
       },
-    },
+    }
+  },
 
-    data: () => ({
-      name: '',
-      email: '',
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: false,
-    }),
+  data() {
+    return {
+      titleTotalCaracters: 30,
+      formData: {
+        title: null,
+        description: null,
+        imagem: "service03.jpg",
+        openingHours: null,
+        stars: 4,
+        cover: "cover08.jpg"
+      },
+    }
+  },
 
-    computed: {
-      checkboxErrors () {
-        const errors = []
-        if (!this.$v.checkbox.$dirty) return errors
-        !this.$v.checkbox.checked && errors.push('You must agree to continue!')
-        return errors
-      },
-      selectErrors () {
-        const errors = []
-        if (!this.$v.select.$dirty) return errors
-        !this.$v.select.required && errors.push('Item is required')
-        return errors
-      },
-      nameErrors () {
-        const errors = []
-        if (!this.$v.name.$dirty) return errors
-        !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-        !this.$v.name.required && errors.push('Name is required.')
-        return errors
-      },
-      emailErrors () {
-        const errors = []
-        if (!this.$v.email.$dirty) return errors
-        !this.$v.email.email && errors.push('Must be valid e-mail')
-        !this.$v.email.required && errors.push('E-mail is required')
-        return errors
-      },
-    },
+  computed: {
+    checkboxErrors() {
+      const errors = []
+      const field = this.$v.formData.checkbox
+      if (field.$dirty) {
+        !field.checked && errors.push("Você Precisa Aceitar os Termos de Uso Para Continuar.")
+        return errors;
+      }
 
-    methods: {
-      submit () {
-        this.$v.$touch()
-      },
-      clear () {
-        this.$v.$reset()
-        this.name = ''
-        this.email = ''
-        this.select = null
-        this.checkbox = false
-      },
+      return errors
     },
-  }
+    titleErrors() {
+      const errors = []
+      const field = this.$v.formData.title
+
+      if (field.$dirty) {
+        !field.maxLength && errors.push(`Digite no Máximo ${this.titleTotalCaracters} Caracteres.`)
+        !field.required && errors.push("Titulo do Anúncio é Obrigatorio.")
+        return errors
+      }
+
+      return errors
+    },
+    isFormValid: () => Object.keys(this.formData).every(field => this.formData[field].valid)
+  },
+  methods: {
+    submit() {
+      this.$v.$touch()
+      if (this.$v.$anyError === false)
+        ServiceProvider.post(this.formData)
+    },
+  },
+}
 </script>
+<style lang="scss" scoped>
+.dg-card {
+  padding: 30px
+}
+.dg-input {
+  margin-top: 10px
+}
+</style>
